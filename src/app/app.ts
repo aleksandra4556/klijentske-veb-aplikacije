fb4a84a6d1410fd540a6fb83c3957d3301757b44
Aleksandra <aleksandrastjepanovic9@gmail.com>
2026-02-25 16:12:58 +0100

    added login

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     RouterLinkWithHref,
     MatButtonModule,
     MatMenuModule,
-    MatToolbarModule
-  ],
+    MatToolbarModule,
+    MatIcon
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
